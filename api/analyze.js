a317ca1e7dd8216f8f8acd7c655a657b652907ac
daonlove13 <daonlove13@gmail.com>
2026-05-13 20:43:59 +0900

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -35,7 +35,7 @@ export default async function handler(req, res) {
         }],
         generationConfig: {
           temperature: 0.3,
-          maxOutputTokens: 500
+          maxOutputTokens: 1500
         }
       })
     });
